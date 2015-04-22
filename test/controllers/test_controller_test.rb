31d543c1f6e419967e7fe977a998c9aae9ec9a8e
Andrew Mutz <andrew.mutz@gmail.com>
2015-04-22 15:44:46 -0700

    Demonstrating how to log in as a user in a devise test
    Also adding emails to the users.yml fixture file because that was
    failing for me.

--- test/controllers/test_controller_test.rb
+++ test/controllers/test_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class TestControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   test "should get index" do
+    sign_in User.first
     get :index
     assert_response :success
   end
