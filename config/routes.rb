f808c46150352c28fdc6f0badba2a56867d0f422
Yuanzhi Gao <billmaxwell@ucla.edu>
2015-05-10 18:47:08 -0700

    Add homepage scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :managers
   devise_for :users
   resources :lines
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
