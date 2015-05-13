3c756abf97ae5f394cfc33a1e429fa2f57550001
jzblue <jundazhucn@yahoo.com>
2015-05-13 16:21:01 -0700

    add front-end assets

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,7 @@
+
+// $.each({{fake_userlist}}, function(i, line) {
+//             console.log(JSON.stringify(line));
+//             var lineContent = document.getElementById("myline");
+//             lineContent.empty();
+
+// });
