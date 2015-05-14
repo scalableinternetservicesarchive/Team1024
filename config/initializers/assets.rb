088c266493dcd30d4aebd119124f43cbf6b5c9ea
jzblue <jundazhucn@yahoo.com>
2015-05-13 18:10:24 -0700

    landing page UI

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
+Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
