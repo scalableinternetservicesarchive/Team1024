2db1c6d2a18dd9983a183a248b139186fa47dfc7
jzblue <jundazhucn@yahoo.com>
2015-05-18 20:33:20 -0700

    more event and line front end work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    if current_user != nil
-      redirect_to user_url(current_user.id)
-      return 
-    end
+    # if current_user != nil
+    #   redirect_to user_url(current_user.id)
+    #   return 
+    # end
 
-    if current_manager != nil
-      redirect_to manager_url(current_manager.id)
-    end
+    # if current_manager != nil
+    #   redirect_to manager_url(current_manager.id)
+    # end
   end
 end
