d9b77993c48811488ebd1ba087d2acfc500bd2d4
Xi Han <xihan94@ucla.edu>
2015-05-20 15:58:58 -0700

    Remove unnecessary javascripts.

--- app/assets/javascripts/dashboard.js
+++
@@ -1,7 +0,0 @@
-
-$.each({{fake_userlist}}, function(i, line) {
-            console.log(JSON.stringify(line));
-            var lineContent = document.getElementById("myline");
-            lineContent.empty();
-
-});
