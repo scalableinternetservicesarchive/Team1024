c4a240baa7d1bd1bfffda3ce98fdf19a3cf122b2
jzblue <jundazhucn@yahoo.com>
2015-05-21 00:03:35 -0700

    seed adding + small fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-#user = User.create()
+
+#user_a = User.create(email: 'ua@b.c', password: 'ua', password_confirmation: 'ua')
+
+#manager_a = Manager.create(email: 'a@b.c', password: '11111111', password_confirmation: '11111111')
+
+#event_1 = Event.create(manager_id: name: 'event_1', max_attendance: 100, start_time: DateTime.new(2015,6,1), create_time: DateTime.new(2015,4,1), description: "text seed event 2")
