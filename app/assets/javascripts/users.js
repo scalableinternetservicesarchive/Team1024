e7bda8b956973d916dbfbe13d89a813e866b0e82
Xi Han <xihan94@ucla.edu>
2015-05-28 11:17:20 -0700

    Implement a countdown for the checkin notification.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,35 @@
 $(document).ready(function() {
   if ($("body").attr("id") == "users") {
+    var pusher = new Pusher('028a87de58f9d41e6158');
+    var channel = pusher.subscribe('notification');
+    channel.bind('new_notification', function(data) {
+      prompt_user('myModal', data.message, 30000);
+    });
   }
 });
 
-function on_users_ui_update() {
-  $('a.save-button').click(function(e) {
-    $(this).addClass('save-button-just-clicked');
-  });
+function prompt_user(modal_id, message, timeout) {
+  $('#' + modal_id + ' .modal-body').html(message);
+
+  var dismiss_text = $('#' + modal_id + ' .modal-footer button[data-dismiss]').html();
+  console.log(dismiss_text);
+  $('#' + modal_id + ' .modal-footer button[data-dismiss]').html(dismiss_text + ' (' + timeout / 1000 + ')');
+
+  $('#' + modal_id).modal('show');
+  
+  setTimeout(function() { 
+    modal_dismiss_timeout_handler(modal_id, timeout, dismiss_text)
+  }, 1000);
+}
+
+// Assume timeout is integer multiple of 1000.
+function modal_dismiss_timeout_handler(modal_id, timeout, dismiss_text) {
+  if (timeout >= 1000) {
+    $('#' + modal_id + ' .modal-footer button[data-dismiss]').html(dismiss_text + ' (' + (timeout / 1000 - 1) + ')');
+      setTimeout(function() { 
+        modal_dismiss_timeout_handler(modal_id, timeout - 1000, dismiss_text)
+      }, 1000);
+  } else {
+    $('#myModal').modal('hide');
+  }
 }
