4efe47dc37886a278fa5e457caa0e135b8bf36cc
Yuanzhi Gao <billmaxwell@ucla.edu>
2015-06-03 14:48:10 -0700

    ignore mail failure msg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
