795fa715bdf614f457f9a0b2ccad0ed64c0e5fa7
Yuanzhi Gao <billmaxwell@ucla.edu>
2015-06-04 00:20:38 -0700

    serverside cache: fragment caching

--- app/helpers/managers_helper.rb
+++ app/helpers/managers_helper.rb
@@ -1,2 +1,13 @@
 module ManagersHelper
+  def cache_key_for_all_event(event)
+    "all_event-#{event.name}-#{event.start_time.to_s}-#{event.description}"
+  end
+
+  def cache_key_for_current_event(event)
+    "current_event-#{event.name}-#{event.start_time.to_s}-#{event.description}"
+  end
+
+  def cache_key_for_past_event(event)
+    "past_event-#{event.name}-#{event.start_time.to_s}-#{event.description}"
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def cache_key_for_favorite_event(event)
+    "favorite_event-#{event.id.to_s}-#{event.name}-#{event.start_time.to_s}-#{event.description}"
+  end
+
+  def cache_key_for_attending_event(event)
+    "attending_event-#{event.name}-#{event.start_time.to_s}-#{event.description}"
+  end
+
+  def cache_key_for_search_event(event)
+    "search_event-#{event.name}-#{event.start_time.to_s}-#{event.description}"
+  end
 end
